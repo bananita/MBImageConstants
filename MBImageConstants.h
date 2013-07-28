871d0c8ab60886b097cc44213f06e751828499da
Michał Banasiak <m.banasiak@icloud.com>
2013-07-28 11:53:27 +0200

    Update MBImageConstants.h

--- MBImageConstants.h
+++ MBImageConstants.h
@@ -1,1 +1,11 @@
-// MBImageConstants
+//
+//  MBImageConstants.h
+//  MBImageConstants
+//
+//  Created by Michał Banasiak on 27.07.2013.
+//  Copyright (c) 2013 Michał Banasiak. All rights reserved.
+//  https://github.com/bananita
+
+/*  
+ *  This file will be filled with proper defines after first build.
+ */
