957648d6e3821b49132de935532e9bf266632018
Michał Banasiak <bananita@users.noreply.github.com>
2014-02-12 23:42:22 +0100

    Update imageconsts.rb

--- imageconsts.rb
+++ imageconsts.rb
@@ -11,28 +11,6 @@
 
 root = ARGV[0]
 
-class Image
-    attr_accessor :normal, :retina
-end
-
-hash = {}
-
-Dir.glob("#{root}/**/*.png") do |file|
-    basename = File.basename(file, ".png")
-    basenameWithoutAtTwo = basename.chomp("@2x")
-    
-    key = basenameWithoutAtTwo
-    
-    hash[key] = Image.new if hash[key].nil?
-
-    if basename.include? "@2x" then
-        hash[key].retina = :true
-    else
-        hash[key].normal = :true
-    end
-    
-end
-
 $stdout.reopen("#{root}/Pods/MBImageConstants/MBImageConstants.h",'w')
 
 puts "//"
@@ -52,23 +30,14 @@ puts " * MBImageConstants tool from the images it found."
 puts " * It should not be modified by hand."
 puts " */"
 puts
+puts
 
-hash.each_key do |key|
-    define = key.gsub('-', '_')
-        
-    normal = "Normal" if hash[key].normal
-    retina = "Retina" if hash[key].retina
+Dir.glob("#{root}/**/*.imageset") do |imageset|
+    name = File.basename(imageset, ".imageset")
+    define = name.gsub ' ', ''
     
-    if (normal.nil?)
-        STDERR.puts "warning: MBImageConstants: missing normal graphics for image #{key}"
-    end
-    
-    if (retina.nil?)
-        STDERR.puts "warning: MBImageConstants: missing retina graphics for image #{key}"
-    end
-    
-    puts "// #{key}: #{normal} #{retina}"
-    puts "#define image_#{define} @\"#{key}\""
+    puts "// #{name}"
+    puts "#define image_#{define} @\"#{name}\""
     puts
 end
 
